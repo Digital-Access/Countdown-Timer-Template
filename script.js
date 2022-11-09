9e1300c011d0c8c0a5e4b9c33078275135799060
Digital-Access <102788087+Digital-Access@users.noreply.github.com>
2022-11-09 08:49:57 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function countdown() {
-    const countDate = new Date("November 08, 2022 08:30:00").getTime();
+    const countDate = new Date("November 15, 2022 08:00:00").getTime();
     const now = new Date().getTime();
     const gap = countDate - now;
 
